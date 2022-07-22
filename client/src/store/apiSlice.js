f540ef98c4d8b3f572ba69f88cf37b86026aee77
Saksham Gupta <saksham.gupta2020@vitbhopal.ac.in>
2022-07-22 13:25:20 +0530

    made some changes for heroku

--- client/src/store/apiSlice.js
+++ client/src/store/apiSlice.js
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const baseURI = 'http://localhost:8080';
+// const baseURI = 'http://localhost:8080';
+const baseURI = 'https://mernexpensetracker92.herokuapp.com';
 
 export const apiSlice = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: baseURI }),
--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 
-require('dotenv').config({path: "./config.env"});
+require('dotenv').config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 
 // use middlewares
@@ -15,13 +16,31 @@ const con = require('./db/connection');
 // Using Routes
 app.use(require('./routes/route'));
 
+if (process.env.NODE_ENV == 'production') {
+    // app.use(express.static(path.join(__dirname, '../client/build')));
+
+    // app.get('/', function(req, res){
+    //     res.sendFile('D: /Study/My Projects/MERN Expense Tracker/client/build/index.html');
+    // });
+    app.get('/', function (req, res) {
+        res.send('API running');
+    });
+}
+else {
+    app.get('/', function (req, res) {
+        res.send('API running');
+    });
+}
+
 con.then(db => {
-    if(!db) return process.exit(1);
-    else{
+    if (!db) return process.exit(1);
+    else {
         // listen to the http server
-        app.listen(port, function(err) {
-            if(err) {console.error(err);}
-            else {console.log( `Server is running on: http://localhost:${port}` );}
+        app.listen(port, function (err) {
+            if (err) { console.error(err); }
+            else {
+                console.log(`Server is running on: http://localhost:${port}`);
+            }
         });
         app.on('error', err => console.log("Failed to Connect with HTTP Server: " + err));
     }
