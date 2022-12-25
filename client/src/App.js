969cbc4f66af0a2eddd57b34a1758dc86bb0683b
Saksham Gupta <saksham.gupta2020@vitbhopal.ac.in>
2022-12-25 18:26:46 +0530

    base template

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import Graph from './components/Graph';
-import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
-      <div className="container mx-auto max-w-6xl text-center drop-shadow-lg text-gray-800">
-        <h1 className="text-4xl py-8 mb-10 bg-purple text-white rounded">Expense Tracker</h1>
-
-        {/* grid columns */}
-        <div className="grid md:grid-cols-2" gap-4>
-          {/* Chart */}
-          <Graph></Graph>
-          {/* Form */}
-          <Form></Form>
-        </div>
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
