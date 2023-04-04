6eb3a3e5532232db514d8dc1b2a06b8fdc14cd14
amaankhanakz <sameeramaan2014@gmail.com>
2023-04-05 01:25:51 +0530

    register-as-professional-starting

---
+++ client/src/components/RegisterasProfessional/RegisterasProfessional.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import WhyPartner from "./WhyPartner";
+import Banner from "./Banner";
+
+function RegisterasProfessional() {
+  return (
+    <>
+      <Banner />
+      <WhyPartner />
+    </>
+  );
+}
+
+export default RegisterasProfessional;
