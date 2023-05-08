a229f0690e4cc962b527378223f05c9b7d2a99c1
amaankhanakz <sameeramaan2014@gmail.com>
2023-05-08 10:31:44 +0530

    carousel_added

--- client/src/components/Homepage/Homepage.jsx
+++ client/src/components/Homepage/Homepage.jsx
@@ -3,17 +3,19 @@ import JoinOurTeam from "./Components/JoinOurTeam/JoinOurTeam";
 import OneClick from "./Components/OneClick/OneClick";
 import OurServices from "./Components/OurServices/OurServices";
 import "./Homepage.css";
+import Carousel from "./Components/Carousel/Carousel";
 
 function Homepage() {
-    return (
-        <>
-            <div className="homepage_content">
-                <OurServices />
-                <OneClick />
-                <JoinOurTeam />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="homepage_content">
+        <Carousel />
+        <OurServices />
+        <OneClick />
+        <JoinOurTeam />
+      </div>
+    </>
+  );
 }
 
 export default Homepage;
